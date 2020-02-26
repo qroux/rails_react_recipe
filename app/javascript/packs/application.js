e5369a959fa18b4b50cee23d369d9a8b480c76d2
quentin roux <quentin.roux@hotmail.fr>
2020-02-26 22:02:40 +0100

    bootstrap fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import 'css/site'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
